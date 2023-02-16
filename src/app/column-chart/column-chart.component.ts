12f2930dcda2c203b4e5fb5d22bc8d42d6d79d58
antithezis <juandbm2001@gmail.com>
2023-02-16 18:36:26 -0500

    Ajuste version angular

--- src/app/column-chart/column-chart.component.ts
+++ src/app/column-chart/column-chart.component.ts
@@ -118,6 +118,7 @@ export class ColumnChartComponent implements OnInit {
 
   constructor(private chartService: ChartsService) {
     if (typeof Highcharts === 'object') {
+
       draggable(Highcharts);
     }
   }
