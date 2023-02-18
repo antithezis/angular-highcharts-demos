e9800aecd167a5dd54dd6bcabadb1daca900c566
antithezis <juandbm2001@gmail.com>
2023-02-18 12:04:26 -0500

    Avance grafica

--- src/app/services/charts.service.ts
+++ src/app/services/charts.service.ts
@@ -17,6 +17,9 @@ export class ChartsService {
 
   buildColumnChart(categories: any, porcentajes: any) {
 
+
+    console.log(categories, porcentajes, "Datos desde el servico")
+
     console.log(porcentajes)
 
     this.seriesOptions = {
@@ -44,11 +47,11 @@ export class ChartsService {
       },
 
       title: {
-        text: 'Efficiency Optimization by Branch',
+        text: 'Simulation chart',
       },
       xAxis: {
         max: 30,
-        categories: categories
+        type: 'category'
       },
       yAxis: [
         {
@@ -78,6 +81,7 @@ export class ChartsService {
         shared: false,
         backgroundColor: '#001231',
       },
+
       plotOptions: {
         column: {
           turboThreshold: 50000000,
@@ -122,6 +126,113 @@ export class ChartsService {
               color: '#F47A04'
             }
           }
+        },
+      },
+    }
+
+    return this.seriesOptions
+
+  }
+
+  buildTwinChart(categories, porcentajes) {
+   this.seriesOptions = {
+      accessibility: {
+        enabled: true,
+      },
+      chart: {
+        zooming: {
+          type: 'xy'
+        },
+        type: 'column',
+        events: {
+          load: function() {
+            //alert('Cargando...');
+          },
+        },
+      },
+
+      credits: {
+        enabled: false
+      },
+
+      navigator: {
+        enabled: true
+      },
+
+      title: {
+        text: 'Twin chart',
+      },
+      xAxis: {
+        max: 30,
+        categories: categories
+      },
+      yAxis: [
+        {
+          min: 0,
+          max: 100,
+          labels: {
+            format: '{value}%',
+          },
+          title: {
+            text: 'Porcentaje de utilización',
+          },
+        },
+      ],
+      series: porcentajes,
+      legend: {
+        shadow: false,
+      },
+      tooltip: {
+        useHTML: true,
+        headerFormat:
+          '<table><tr><th style="color:#FFFFFF"colspan="2">{point.key}</th></tr>',
+        pointFormat:
+          // '<tr><td style="color: {series.color}">{series.name} </td>' +
+          '<tr><td style="color:#FFFFFF">{series.name} </td>' +
+          '<td style="text-align: right; color:#FFFFFF"><b>{point.y} %</b></td></tr>',
+        footerFormat: '</table>',
+        shared: false,
+        backgroundColor: '#001231',
+      },
+      plotOptions: {
+        column: {
+          turboThreshold: 50000000,
+          grouping: false,
+          shadow: false,
+          borderWidth: 1.5,
+          stacking: 'percent',
+          opacity: 0.8,
+          pointWidth: 13,
+          pointPadding: 0.3,
+          color: '#5574A8',
+        },
+        series: {
+          // stickyTracking: false,
+          // point: {
+          //   events: {
+          //     drop: function(e: any) {
+          //       this.update({
+          //         color: '#F47A04',
+          //       });
+          //     },
+          //     click: function() {
+          //     },
+          //     dragStart: function(e: any) {
+          //     },
+          //     drag: function(e: any) {
+          //     },
+          //     mouseOver: function(e: any) {
+          //     },
+          //     // click: function (e: any) {
+          //     //   debugger;
+          //     // },
+          //   },
+          // },
+          // states: {
+          //   hover: {
+          //     color: '#F47A04'
+          //   }
+          // }
         },
       },
     }
--- src/app/twin-column-chart/twin-column-chart.component.ts
+++ src/app/twin-column-chart/twin-column-chart.component.ts
@@ -1,4 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import { Welcome } from '../intefaces/columnChartData';
+import { ChartsService } from '../services/charts.service';
+//import HighchartsReact from 'highcharts-react-official';
+
 
 @Component({
   selector: 'app-twin-column-chart',
@@ -7,9 +13,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TwinColumnChartComponent implements OnInit {
 
-  constructor() { }
+  dataChart: Welcome[]
+  categoires: any[]
+
+  viewChart: boolean
+
+  Highcharts: typeof Highcharts = Highcharts;
+  columnchart: Highcharts.Options = {};
+
+  constructor(private chartService: ChartsService) {
+    if (typeof Highcharts === 'object') {
+      // draggable(Highcharts);
+    }
+  }
 
   ngOnInit(): void {
+    this.getData()
+    this.viewChart = false
+  }
+
+  getData() {
+    this.chartService.getData().subscribe({
+      next: data => {
+        this.dataChart = data.map((x: any) => ({
+          categories: x.machineName,
+          procentajes: Object.values(x.data.reduce((accumulator: any, currentValue: any) => {
+            const robotName = currentValue.robotName;
+            if (!accumulator[robotName]) {
+              accumulator[robotName] = {
+                name: robotName,
+                type: 'column',
+                data: []
+              };
+            }
+            accumulator[robotName].data.push(currentValue.percentage);
+            return accumulator;
+          }, {}))
+        }));
+
+        this.categoires = this.dataChart.map((x: any) => x.categories)
+
+        const melapela = this.dataChart.reduce((acc, val) => acc.concat(val), []);
+
+        this.buildData(melapela)
+      }
+    })
+  }
+
+  buildData(melapela: any) {
+    let test = melapela.map((x: any) => x.procentajes)
+    let porcentajes = test.reduce((acc, val) => acc.concat(val), [])
+    this.showChart(this.categoires, porcentajes)
+  }
+
+  showChart(categories: any, procentajes: any): void {
+    this.columnchart = this.chartService.buildTwinChart(categories, procentajes)
+    this.viewChart = true
   }
 
 }
